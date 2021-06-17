4d23265f4a92ae4d755c468a9024f2e8b5e584ee
Matthew Berginski <matthew.berginski@gmail.com>
2021-06-17 15:13:22 -0400

    adding single model files for glm testing

---
+++ src/klaeger_synergy_regression_model/single_model_scripts/glm_model_CAF.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(here)
+library(tidymodels)
+library(tictoc)
+library(doParallel)
+
+tic()
+doParallel::registerDoParallel(cores=detectCores() - 2)
+
+cell_line_compound_splits = read_rds(here('results/klaeger_synergy_regression_CV_split.rds'))
+
+norm_recipe <- 
+	recipe(
+		viability ~ ., 
+		data = cell_line_compound_splits$CAF$splits[[1]]$data
+	) %>%
+	step_center(all_predictors()) %>%
+	step_scale(all_predictors()) %>%
+	# estimate the means and standard deviations
+	prep(training = cell_line_compound_splits$CAF$splits[[1]]$data, retain = TRUE)
+
+glm_spec <- linear_reg(
+	penalty = tune(), 
+	mixture = tune()
+) %>% set_engine("glmnet") %>% 
+	set_mode("regression")
+
+glm_grid <- grid_latin_hypercube(
+	penalty(),
+	mixture(),
+	size = 100
+)
+
+glm_wf <- workflow() %>%
+	add_recipe(norm_recipe) %>%
+	add_model(glm_spec)
+
+tune_grid(
+	glm_wf,
+	resamples = cell_line_compound_splits$CAF,
+	grid = glm_grid,
+	control = control_grid(save_pred = TRUE)
+) %>% write_rds(here('results/glm_regression_models/CAF.rds'), compress = 'gz')
+toc()

401f0462258d1d06e8e11e134a7dfade9918c375
Matthew Berginski <matthew.berginski@gmail.com>
2021-06-23 11:40:59 -0400

    adding NN model files/submission script

--- src/klaeger_synergy_regression_model/single_model_scripts/submit_ML_models.sh
+++ src/klaeger_synergy_regression_model/single_model_scripts/submit_ML_models.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-# sbatch -c 64 --time=24:00:00 --wrap="./rand_forest_model_CAF.R"
-# sbatch -c 64 --time=24:00:00 --wrap="./rand_forest_model_P1004.R"
-# sbatch -c 64 --time=24:00:00 --wrap="./rand_forest_model_P1304.R"
-# 
-# sbatch -c 64 --time=24:00:00 --wrap="./svm_model_CAF.R"
-# sbatch -c 64 --time=24:00:00 --wrap="./svm_model_P1004.R"
-# sbatch -c 64 --time=24:00:00 --wrap="./svm_model_P1304.R"
-# 
-# sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_CAF.R"
-# sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_P1004.R"
-# sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_P1304.R"
+sbatch -c 64 --time=24:00:00 --wrap="./rand_forest_model_CAF.R"
+sbatch -c 64 --time=24:00:00 --wrap="./rand_forest_model_P1004.R"
+sbatch -c 64 --time=24:00:00 --wrap="./rand_forest_model_P1304.R"
+
+sbatch -c 64 --time=24:00:00 --wrap="./svm_model_CAF.R"
+sbatch -c 64 --time=24:00:00 --wrap="./svm_model_P1004.R"
+sbatch -c 64 --time=24:00:00 --wrap="./svm_model_P1304.R"
+
+sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_CAF.R"
+sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_P1004.R"
+sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_P1304.R"
 
 sbatch -c 64 --time=24:00:00 --wrap="./glm_model_CAF.R"
 sbatch -c 64 --time=24:00:00 --wrap="./glm_model_P1004.R"
 sbatch -c 64 --time=24:00:00 --wrap="./glm_model_P1304.R"
+
+sbatch -c 4 --time=72:00:00 --wrap="./NN_model_CAF.R"
+sbatch -c 4 --time=72:00:00 --wrap="./NN_model_P1004.R"
+sbatch -c 4 --time=72:00:00 --wrap="./NN_model_P1304.R"

62dfd767ecb94c364d572fa657701e9906c99fff
Matthew Berginski <matthew.berginski@gmail.com>
2021-06-23 10:41:35 -0400

    adding NN version of binary classification

---
+++ src/klaeger_synergy_binary_model/single_model_scripts/NN_model_P1304.R
@@ -0,0 +1,40 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(here)
+library(tidymodels)
+library(tictoc)
+library(doParallel)
+
+tic()
+
+doParallel::registerDoParallel(cores=detectCores() - 2)
+
+cell_line_compound_splits = read_rds(here('results/klaeger_synergy_classification_90_CV_split.rds'))
+
+nn_spec = mlp(
+	epochs = tune(),
+	hidden_units = tune(),
+	dropout = tune()
+) %>% set_engine("keras", verbose = 0) %>%
+	set_mode("classification")
+
+nn_grid = grid_latin_hypercube(
+	epochs(),
+	hidden_units(),
+	dropout(),
+	size = 100
+)
+
+nnet_wf <- workflow() %>%
+	add_formula(viability_90 ~ .) %>%
+	add_model(nn_spec)
+
+tune_grid(
+	nnet_wf,
+	resamples = cell_line_compound_splits$P1304,
+	grid = nn_grid,
+	control = control_grid(save_pred = TRUE)) %>% 
+	write_rds(here('results/NN_below90_models/P1304.rds'), compress = 'gz')
+
+toc()
--- src/klaeger_synergy_binary_model/single_model_scripts/submit_ML_models.sh
+++ src/klaeger_synergy_binary_model/single_model_scripts/submit_ML_models.sh
@@ -11,3 +11,7 @@ sbatch -c 64 --time=24:00:00 --wrap="./svm_model_P1304.R"
 sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_CAF.R"
 sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_P1004.R"
 sbatch -c 64 --time=24:00:00 --wrap="./xgboost_model_P1304.R"
+
+sbatch -c 64 --time=24:00:00 --wrap="./NN_model_CAF.R"
+sbatch -c 64 --time=24:00:00 --wrap="./NN_model_P1004.R"
+sbatch -c 64 --time=24:00:00 --wrap="./NN_model_P1304.R"

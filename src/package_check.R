f739ab8136bec447c15291524a2d6a35009181ba
Matthew Berginski <matthew.berginski@gmail.com>
2021-07-02 11:27:25 -0400

    adding script to check for and install needed packages

---
+++ src/package_check.R
@@ -0,0 +1,31 @@
+if("pacman" %in% rownames(installed.packages()) == FALSE) {
+	install.packages("pacman")
+}
+
+library(pacman)
+
+p_load(rmarkdown)
+p_load(broom) 
+p_load(doParallel) 
+p_load(ggpubr)
+p_load(ggridges) 
+p_load(ggupset)
+p_load(gridExtra)
+p_load(gt)
+p_load(here)
+p_load(janitor)
+p_load(MESS)
+p_load(patchwork)
+p_load(readxl)
+p_load(ROCR)
+p_load(tictoc)
+p_load(tidymodels)
+p_load(tidyverse)
+p_load(vip)
+p_load(yardstick)
+p_load(ranger)
+p_load(kernlab)
+p_load(xgboost)
+
+p_load_gh('mbergins/BerginskiRMisc')
+p_load_gh('IDG-Kinase/DarkKinaseTools')
